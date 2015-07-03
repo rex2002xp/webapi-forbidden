4b72e383514c8aecffbac94bb7a15624e763851f
Victor Cornejo <rex2002xp@gmail.com>
2015-07-03 00:35:12 -0600

    Dia con dia las aplicaciones que consumen servicios de tipo WebApi crecen, y como desarrolladores estamos llamados a profundizar en el conocimiento del protocolo HTTP. Revisemos algunos conceptos muy utilices para el manejo del código 403 Forbidden.

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebApi.HandlesMessageHttp;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/values")]
+    public class ValuesController : ApiController
+    {
+        private List<string> data = new List<string>() { "Valor 1", "Valor 2" };
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(data);
+        }
+
+        [CustomAuthorize(Roles = "Admin")]
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            data.RemoveAt(id);
+            return Ok(data);
+        }
+    }
+}
---
+++ WebApi/HandlesMessageHttp/CustomAuthorize.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.HandlesMessageHttp
+{
+    public class CustomAuthorize : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            if (((System.Web.HttpContext.Current.User).Identity).IsAuthenticated)
+            {
+                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(actionContext);
+            }
+        }
+    }
+}
